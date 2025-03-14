832aee7851378df278ef9d5e45b36252bdad36e6
JamesTheButler <ille.tom@outlook.com>
2025-03-14 11:22:08 +0100

    fix all wobble

---
+++ Assets/Code/FloatExtensions.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Code
+{
+    public static class FloatExtensions
+    {
+        public static bool IsApproximatelyZero(this float value)
+        {
+            return Mathf.Approximately(value, 0f);
+        }
+
+        /// <summary>
+        ///  Normalizes an angle to the range [-180, 180] degrees.
+        /// </summary>
+        public static float NormalizeAngle(this float angle)
+        {
+            while (angle > 180f) angle -= 360f;
+            while (angle < -180f) angle += 360f;
+            return angle;
+        }
+    }
+}
--- Assets/Code/GliderController.cs
+++ Assets/Code/GliderController.cs
@@ -1,20 +1,36 @@
-using Unity.Mathematics.Geometry;
 using UnityEngine;
 
 namespace Code
 {
     public class GliderController : MonoBehaviour
     {
-        [SerializeField] private float defaultMoveSpeedInSec = 5f;
-        [SerializeField] private float maxRollAngleInDeg = 45f;
-        [SerializeField] private float yawSpeedInDegPerSec = 2f;
-        [SerializeField] private float rollSpeedInDegPerSec = 2f;
-        [SerializeField] private float boostMultiplier = 2f;
+        [SerializeField]
+        private float defaultMoveSpeedInSec = 5f;
+
+        [SerializeField]
+        private float yawSpeedInDegPerSec = 2f;
+
+        [SerializeField]
+        private float boostMultiplier = 2f;
+
+        [Header("Roll")]
+        [SerializeField]
+        private float rollSpeedInDegPerSec = 2f;
+
+        [SerializeField]
+        private float rollResetSpeedInDegPerSec = 2f;
+
+        [SerializeField]
+        private float maxRollAngleInDeg = 45f;
+
+        private Inputs _currentInputs = new(0, 0);
 
         private FlightInputs _flightInputs;
-
-        private Inputs _inputs = new(0, 0);
         private bool _isBoosting;
+
+        private int i;
+
+        private float CurrentRoll => transform.localRotation.eulerAngles.z.NormalizeAngle();
 
         private void Start()
         {
@@ -26,7 +42,7 @@ namespace Code
             var moveSpeed = _isBoosting ? defaultMoveSpeedInSec * boostMultiplier : defaultMoveSpeedInSec;
             transform.position += transform.forward * (moveSpeed * Time.deltaTime);
 
-            ApplySteer();
+            ApplyInputs();
         }
 
         private void OnDestroy()
@@ -35,10 +51,113 @@ namespace Code
             _flightInputs.BoostChanged -= OnFlightInputsOnBoostChanged;
         }
 
+        private void ApplyInputs()
+        {
+            var inputDiff = GetInputDiff();
+
+            if (inputDiff.IsApproximatelyZero())
+            {
+                AutoLevel();
+                return;
+            }
+
+            Steer(inputDiff);
+        }
+
+        private void AutoLevel()
+        {
+            var baseRoll = rollResetSpeedInDegPerSec * Time.deltaTime;
+            var currentRoll = CurrentRoll;
+
+            if (currentRoll.IsApproximatelyZero())
+            {
+                return;
+            }
+
+            var rollAngle = baseRoll * -Mathf.Sign(currentRoll);
+
+            // fix wobble around center
+            if (
+                (currentRoll > 0 &&
+                 currentRoll < baseRoll &&
+                 Mathf.Abs(rollAngle) > currentRoll &&
+                 rollAngle < 0f)
+                ||
+                (currentRoll < 0 &&
+                 Mathf.Abs(currentRoll) < baseRoll &&
+                 Mathf.Abs(rollAngle) > currentRoll &&
+                 rollAngle > 0f)
+            )
+            {
+                rollAngle = -currentRoll;
+            }
+
+            Roll(rollAngle);
+        }
+
+        private void Steer(float inputDiff)
+        {
+            i++;
+            // yaw
+            var yaw = yawSpeedInDegPerSec * Time.deltaTime * -inputDiff;
+            transform.RotateAround(
+                transform.position,
+                Vector3.up,
+                yaw);
+
+            // roll
+            var rollAngle = inputDiff * rollSpeedInDegPerSec * Time.deltaTime;
+            var clampedRollAngle = ClampRollAngle(rollAngle);
+
+            if (Mathf.Abs(CurrentRoll) > 40)
+            {
+                Debug.Log($"[{i}] current {CurrentRoll} + {clampedRollAngle} (initial: {rollAngle})");
+            }
+
+            Roll(clampedRollAngle);
+
+            if (Mathf.Abs(CurrentRoll) > 40)
+            {
+                Debug.Log($"[{i}] new roll {CurrentRoll}");
+            }
+            // clamp roll
+            //var clampedRoll = Mathf.Clamp(CurrentRoll, -maxRollAngleInDeg, maxRollAngleInDeg);
+            //var currentRot = transform.localRotation;
+            //transform.localRotation = Quaternion.Euler(currentRot.x, currentRot.y, clampedRoll);
+        }
+
+        private void Roll(float rollAngle)
+        {
+            transform.RotateAround(
+                transform.position,
+                transform.forward,
+                rollAngle);
+        }
+
+        /// <summary>
+        ///     Clamp desired roll angle if it would over-rotate.
+        /// </summary>
+        private float ClampRollAngle(float rollAngle)
+        {
+            var currentRoll = CurrentRoll;
+            if (currentRoll + rollAngle > maxRollAngleInDeg)
+            {
+                rollAngle = maxRollAngleInDeg - currentRoll;
+            }
+            else if (currentRoll + rollAngle < -maxRollAngleInDeg)
+            {
+                rollAngle = -(maxRollAngleInDeg + currentRoll);
+            }
+
+            return rollAngle;
+        }
+
+        #region Input Detection
+
         private void SetUpFlightControls()
         {
             _flightInputs = GetComponent<FlightInputs>();
-            _inputs = _flightInputs.Inputs;
+            _currentInputs = _flightInputs.Inputs;
             _flightInputs.InputsChanged += OnFlightInputsOnInputsChanged;
             _flightInputs.BoostChanged += OnFlightInputsOnBoostChanged;
         }
@@ -50,94 +169,14 @@ namespace Code
 
         private void OnFlightInputsOnInputsChanged(Inputs inputs)
         {
-            _inputs = inputs;
+            _currentInputs = inputs;
         }
 
-        private void ApplySteer()
+        private float GetInputDiff()
         {
-            var inputDiff = ComputeInputDiff();
-
-            var yaw = yawSpeedInDegPerSec * Time.deltaTime * -inputDiff;
-            transform.RotateAround(
-                transform.position,
-                Vector3.up,
-                yaw);
-
-            var baseRoll = rollSpeedInDegPerSec * Time.deltaTime;
-
-            var rollAngle = inputDiff * baseRoll;
-            rollAngle = ClampRollAngle(rollAngle);
-
-            // apply auto-leveling/auto un-roll
-            if (Mathf.Approximately(rollAngle, 0f))
-            {
-                rollAngle = DisableAutoBalanceWobble(baseRoll);
-            }
-
-            transform.RotateAround(
-                transform.position,
-                transform.forward,
-                rollAngle);
+            return _currentInputs.Right - _currentInputs.Left;
         }
 
-        private float DisableAutoBalanceWobble(float baseRoll)
-        {
-            float rollAngle;
-            var currentRoll = transform.localRotation.eulerAngles.z;
-            var currentNormalizedRoll = NormalizeAngle(currentRoll);
-
-            rollAngle = baseRoll * -Mathf.Sign(currentNormalizedRoll);
-            // fix wobble around center
-            if (
-                (currentNormalizedRoll > 0 &&
-                 currentNormalizedRoll < baseRoll &&
-                 Mathf.Abs(rollAngle) > currentNormalizedRoll &&
-                 rollAngle < 0f)
-                ||
-                (currentNormalizedRoll < 0 &&
-                 Mathf.Abs(currentNormalizedRoll) < baseRoll &&
-                 Mathf.Abs(rollAngle) > currentNormalizedRoll &&
-                 rollAngle > 0f)
-            )
-            {
-                rollAngle = -currentNormalizedRoll;
-            }
-
-            return rollAngle;
-        }
-
-        /// <summary>
-        /// Clamp desired roll angle if it would over-rotate.
-        /// </summary>
-        private float ClampRollAngle(float rollAngle)
-        {
-            var currentRoll = transform.localRotation.eulerAngles.z;
-            var currentNormalizedRoll = NormalizeAngle(currentRoll);
-
-            // if currentRoll == 44 and rollAngle == 2 => rollAngle = 1
-            if (currentNormalizedRoll + rollAngle > maxRollAngleInDeg)
-            {
-                rollAngle = maxRollAngleInDeg - currentNormalizedRoll;
-            }
-            // if currentRoll == -44 and rollAngle == -2 => rollAngle = -1
-            else if (currentNormalizedRoll + rollAngle < -maxRollAngleInDeg)
-            {
-                rollAngle = maxRollAngleInDeg + currentNormalizedRoll;
-            }
-
-            return rollAngle;
-        }
-
-        private float ComputeInputDiff()
-        {
-            return _inputs.Right - _inputs.Left;
-        }
-
-        private static float NormalizeAngle(float angle)
-        {
-            while (angle > 180f) angle -= 360f;
-            while (angle < -180f) angle += 360f;
-            return angle;
-        }
+        #endregion
     }
 }
